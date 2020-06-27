ffef4b00491e134c637ac0da352dbe045a4a7549
Mohit-Swain <43781668+Mohit-Swain@users.noreply.github.com>
2020-06-27 17:53:30 +0530

    added cheemsify function
    it will help you to translate doge English to cheems English.

--- cheems.py
+++ cheems.py
@@ -1,2 +1,19 @@
+import random
+
 def primt(obj):
     print(obj)
+    
+def cheemsify(obj,count_m = 2):
+    if not isinstance(obj,str):
+        raise TypeError("the input type must be a string")
+
+    new_obj = obj
+    for _ in range(count_m):
+        l = len(new_obj)
+        insert_pos = random.randrange(1,l)
+        new_obj = new_obj[0:insert_pos] +'m' + new_obj[insert_pos:l] 
+
+    return new_obj
+
+
+    
